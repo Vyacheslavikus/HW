04c911296eea094fe2e87f3520d7aeff5b939de8
Vyacheslavikus <lapshina90@mail.ru>
2023-09-14 20:07:17 +0300

    init commit

---
+++ Gitdemo/Shared/GitdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitdemoApp.swift
+//  Shared
+//
+//  Created by Слава on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
