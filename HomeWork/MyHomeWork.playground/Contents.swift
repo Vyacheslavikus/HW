a36d181681436f2951b86f8c52de5d69b602218e
Vyacheslavikus <lapshina90@mail.ru>
2023-09-19 19:57:59 +0300

    SecondHW

--- HomeWork/MyHomeWork.playground/Contents.swift
+++ HomeWork/MyHomeWork.playground/Contents.swift
@@ -1,6 +1,12 @@
 import UIKit
+import CoreGraphics
 
 var greeting = "Hello, playground"
 let MyFirstName = "Vyacheslav"
 let MySecondName = "Kocherin"
 print (MyFirstName+" "+MySecondName)
+var number1 = 3
+var number2 = 7
+var summ = number1+number2
+print (summ)
+
